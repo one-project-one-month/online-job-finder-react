612a62d3de10e983e38f521e146ee81c7e4005a1
hmthu <hponemyatthu.ojt@d3-sg.com>
2025-02-22 23:33:26 +0630

    make ojf project layout, mobile base

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,12 @@
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
-import App from "@/App.tsx";
-import "@/index.css";
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import App from '@/App.tsx'
+import '@/index.css'
 
-createRoot(document.getElementById("root")!).render(
+createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <div className="container bg-[#F9F9F9] min-h-screen mx-auto w-auto xl:w-1/2">
+      <App />
+    </div>
   </StrictMode>
-);
+)
