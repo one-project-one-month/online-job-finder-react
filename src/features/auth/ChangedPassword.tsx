84fbe7e38e46896f460fb2ddde7ecd270660a649
techking11 <nma1132000@gmail.com>
2025-02-23 15:32:39 +0700

    :sparkles: feat(component): add ChangedPassword

---
+++ src/features/auth/ChangedPassword.tsx
@@ -0,0 +1,28 @@
+import AuthHead from '@/features/auth/AuthHead'
+import verifyEmailSvg from '@/assets/images/auth/verifymail.svg'
+import { Button } from '@/components/ui/button'
+
+export default function ChangedPassword() {
+  return (
+    <div className="min-h-screen px-7 py-20 text-center md:px-16 md:py-24">
+      <AuthHead
+        headTitle="Successfully"
+        headCaption="Your password has been updated, please change your password regularly to avoid this happening"
+      />
+      <div className="flex items-center justify-center py-28">
+        <img className="w-36 md:w-56" src={verifyEmailSvg} alt="" />
+      </div>
+      <div className="flex flex-col">
+        <Button className="mb-3 bg-[#130160] py-6 text-base uppercase hover:bg-[#130160] md:py-7 md:text-xl">
+          Continue
+        </Button>
+        <Button
+          className="bg-[#d6cdfe] py-6 text-base uppercase hover:bg-[#d6cdfe] md:py-7 md:text-xl"
+          variant="default"
+        >
+          Back to login
+        </Button>
+      </div>
+    </div>
+  )
+}
