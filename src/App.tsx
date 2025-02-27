e74e6cde2100e588c78d724e65930267d14c9a6a
mu phaw <muphaw755@gmail.com>
2025-02-27 17:55:01 +0630

    update admin and public routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 import { Suspense } from 'react'
 import { RouterProvider } from 'react-router-dom'
 import router from './routes/router'
+import LoadingDots from './components/_ui/LoadingDots'
 
 export default function App() {
   return (
     <div>
-    <Suspense fallback={<div>Loading...</div>}>
+    <Suspense fallback={<LoadingDots/>}>
         <RouterProvider router={router} />
     </Suspense>
     </div>
