f51835ede0724e495c07e6f7e57a81f9c41f78a7
mu phaw <muphaw755@gmail.com>
2025-02-27 20:09:42 +0630

    updated

--- src/components/layouts/PublicLayout.tsx
+++ src/components/layouts/PublicLayout.tsx
@@ -3,10 +3,12 @@ import TabBar from '../_ui/TabBar'
 
 const PublicLayout = () => {
   return (
-    <div>
-      <Outlet/>
-      <TabBar/>
-    </div>
+    <div className="flex flex-col h-screen">
+    <div className="flex-1  pb-[52px] md:pb-[60px] lg:pb-[70px]">
+        <Outlet />
+      </div>
+    <TabBar />
+  </div>
   )
 }
 
